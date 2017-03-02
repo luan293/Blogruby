7f9e28eba718180c62978092841a4da479a2734a
luan <luan29394@gmail.com>
2017-03-02 16:16:10 +0700

    validate, add class to div

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,11 @@ class PostsController < ApplicationController
   end
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   def show
     @post = Post.find(params[:id])
