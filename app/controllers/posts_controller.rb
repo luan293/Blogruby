e045b4aac047f93f175ee142a86e45d1ee4ad45e
luan <luan29394@gmail.com>
2017-03-02 16:37:28 +0700

    add func del, edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order('created_at DESC')
   end
+
   def new
     
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -13,11 +15,30 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
   def show
     @post = Post.find(params[:id])
   end
-  def destroy
+
+  def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to @root_path 
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content)
